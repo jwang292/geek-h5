7cca7669fb89ea2323ac098c97471e21e2b4138d
jinfeng <wjinfeng@yahoo.ca>
2022-09-19 22:06:38 -0400

    redux env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import { Button } from 'antd-mobile'
+import React, { Suspense } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch, //多个路由规则
+  Redirect, //重镜像跳转
+  // Link,
+} from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+
+//按需加载
+// const Login = React.lazy(() => {
+//   import('./pages/Login')
+// })
+// const Home = React.lazy(() => {
+//   import('./pages/Home')
+// })
 export default function App() {
   return (
-    <div>
-      <Button block color="primary" size="large">
-        test
-      </Button>
-    </div>
+    <Router>
+      <div className="app">
+        {/* <Link to="./login">login</Link>
+        <Link to="./home">home</Link> */}
+        {/* <Suspense fallback={<div>loading</div>}> */}
+        <Switch>
+          {/* 组件加载中，显示loading */}
+
+          <Redirect exact from="/" to="/home"></Redirect>
+          <Route path="/home" component={Home}></Route>
+          <Route path="/login" component={Login}></Route>
+        </Switch>
+        {/* </Suspense> */}
+      </div>
+    </Router>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 import './assets/styles/index.scss'
+import store from './store'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App></App>, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App></App>
+  </Provider>,
+  document.getElementById('root')
+)
